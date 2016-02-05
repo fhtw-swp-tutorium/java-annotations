b6665b9e3cb42e44d5e8012c1f35b78b576d0f0e
Thomas Eizinger <thomas.eizinger@senacor.com>
2016-02-05 01:34:24 +0100

    initial commit

---
+++ src/main/java/at/uastw/swp/observer/Subject.java
@@ -0,0 +1,39 @@
+package at.uastw.swp.observer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to be used as a marker for the subject of an observer pattern.
+ * If the subject has constructor parameters, you can specify a factory class that should be used during testing to construct a subject instance.
+ * The factory class should have a zero-argument constructor and a single public zero-argument method that returns an instance of the subject.
+ *
+ * The class should have methods for attaching and detaching an observer and notifying all attached observers.
+ * These methods are determine by a naming convention, which allows the following names:
+ * <ul>
+ *     <li>attachXXX</li>
+ *     <li>subscribeXXX</li>
+ *     <li>addXXX</li>
+ *     <li>registerXXX</li>
+ *     <li>detachXXX</li>
+ *     <li>unsubscribeXXX</li>
+ *     <li>removeXXX</li>
+ *     <li>unregisterXXX</li>
+ *     <li>notifyXXX</li>
+ *     <li>updateXXX</li>
+ * </ul>
+ *
+ * @author Thomas Eizinger, Senacor Technologies AG.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Subject {
+
+    Class<?> factory() default Subject.None.class;
+
+    class None {
+
+    }
+}
---
+++ src/main/java/at/uastw/swp/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package at.uastw.swp.singleton;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Thomas Eizinger, Senacor Technologies AG.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Singleton {
+
+}
